247292c7916be3423540d3fdd2b328973f05ecc6
BY <bhanu0120@gmail.com>
2017-11-11 16:14:37 +0000

    Admin dashboard

--- MagicProperties/src/app/app.module.ts
+++ MagicProperties/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LandlorddashboardComponent } from './components/landlorddashboard/landlorddashboard.component';
+import { AdmindashboardComponent } from './components/admindashboard/admindashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LandlorddashboardComponent
+    LandlorddashboardComponent,
+    AdmindashboardComponent
   ],
   imports: [
     BrowserModule
