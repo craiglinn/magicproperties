4c90518c3e17bd889caf98f6ad6fcc0ecbcfebff
jagan <jagadishwar@gmail.com>
2017-11-11 14:55:55 +0000

    added further register changes

--- MagicProperties/src/app/components/register/register.component.ts
+++ MagicProperties/src/app/components/register/register.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  myform: any;
 
+  firstName: string;
+  lastName: string;
+  email: string;
+  telephoneNumber: number;
+  mobileNumber: number;
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmit() {
+    console.log('testing!');
+    if (this.myform.valid) {
+    console.log('Form Submitted!');
+  }
+  }
 }
